35762bc27ca00702e89eae29d553619dbc8759d9
andyantunes <andersonlantunes@gmail.com>
2021-08-20 20:53:06 -0300

    Added cep directive to format cep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CepDirective } from './directives/cep/cep.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CepDirective],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, MatFormFieldModule, MatInputModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/directives/cep/cep.directive.ts
@@ -0,0 +1,63 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Directive({
+  selector: '[cep]',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: CepDirective,
+      multi: true,
+    },
+  ],
+})
+export class CepDirective implements ControlValueAccessor {
+  private onTouched: any;
+  private onChange: any;
+
+  private cepRegex = /^([\d]{5})\-?([\d]{3})/;
+
+  constructor(private el: ElementRef) {}
+
+  /**
+   * Implements the keyup event to the directive element and format cep value - Ex.: (5-3)
+   *
+   * @param $event any
+   */
+  @HostListener('keyup', ['$event']) onKeyUp($event: any) {
+    const value = $event.target.value;
+    const formatted = value.replace(this.cepRegex, '$1-$2');
+    const cepFormatted = formatted.substring(0, 9);
+
+    $event.target.value = cepFormatted;
+  }
+
+  /**
+   * Registra a função a ser chamada para atualizar o valor na model.
+   *
+   * @param fn any
+   */
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  /**
+   * Registra a função a ser chamada para atualizar o valor na model
+   * para o evento touched.
+   *
+   * @param fn any
+   */
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  /**
+   * Obtém o valor contido na model.
+   *
+   * @param value any
+   */
+  writeValue(value: string): void {
+    console.log(value);
+    this.el.nativeElement.value = value;
+  }
+}
